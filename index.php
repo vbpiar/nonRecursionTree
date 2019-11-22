5cfaa8df0ebe4ac53edc42c7791997a9c14b7cb8
vbpiar <37880618+vbpiar@users.noreply.github.com>
2019-11-22 21:08:26 +0200

    Function that allows create tree without recursion

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+$test_array = [
+    ['id'=>1,'name'=>'Jhon','parent_id'=>0],
+    ['id'=>2,'name'=>'Mike','parent_id'=>1],
+    ['id'=>3,'name'=>'Den','parent_id'=>1],
+    ['id'=>4,'name'=>'Ivan','parent_id'=>2],
+    ['id'=>5,'name'=>'Taras','parent_id'=>3],
+    ['id'=>6,'name'=>'Olga','parent_id'=>4],
+    ['id'=>7,'name'=>'Darya','parent_id'=>5],
+    ['id'=>8,'name'=>'Viktoriya','parent_id'=>0],
+
+];
+
+
+function makeTree(&$input_array) {
+
+    $map = [
+        0 =>['catalog' => []]
+    ];
+
+    foreach ($input_array as &$category) {
+        $category['catalog'] = [];
+        $map[$category['id']] = &$category;
+    }
+
+    foreach ($input_array as &$category) {
+        $map[$category['parent_id']]['catalog'][] = &$category;
+    }
+    return $map[0]['catalog'];
+
+}
+
+
+
+$tree = makeTree($test_array);
+
+print_r ($tree);
